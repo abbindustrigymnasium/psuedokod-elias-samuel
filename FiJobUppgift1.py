1109fec92e7bd5cda860f6e4e678bbd140c1b68a
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2018-10-10 12:27:29 +0200

    löst uppgifterna i Python
    Co-Authored-By: Samuel Olsson <abbsamols@users.noreply.github.com>

---
+++ FiJobUppgift1.py
@@ -0,0 +1,10 @@
+
+Månader = [7,8,9] 
+Födelsemånad = 7
+studiebidrag = 1250*10*3+1250*3
+if Födelsemånad in Månader:
+    studiebidraget=studiebidrag-625
+    print(studiebidraget)
+else:
+    print(studiebidrag)
+
---
+++ SpookyScaryUppgift3.py
@@ -0,0 +1,12 @@
+AntalBiljetter=20
+Galnamusen=3
+Flygandekvasten=4
+Lustigahuset=2
+
+Kvot1 = AntalBiljetter/Galnamusen 
+Kvot2 = AntalBiljetter/Flygandekvasten
+Kvot3 = AntalBiljetter/Lustigahuset 
+
+print("Åsa kan åka Galna Musen", str(int(Kvot1)) ,"gånger")
+print("Åsa kan åka Flygande Kvasten", str(int(Kvot2)) ,"gånger")
+print("Åsa kan gå i Lustiga Huset", str(int(Kvot3)) ,"gånger")
